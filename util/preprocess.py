3372ab190f95f39cd200d7d169ca5106275914d7
M216916 <86331863+M216916@users.noreply.github.com>
2022-09-22 19:29:50 +0900

    Update preprocess.py

--- util/preprocess.py
+++ util/preprocess.py
@@ -82,35 +82,34 @@ def construct_data(data, feature_map, labels=0):
 
     return res
 
-def build_loc_net(struc, all_features, feature_map=[]):
+def build_loc_net(struc, all_features, feature_map=[]):          # struc : 27組のfeature ／ all_features, feature_map : 27のfeature
 
     index_feature_map = feature_map
     edge_indexes = [
         [],
         []
     ]
-    for node_name, node_list in struc.items():
-        if node_name not in all_features:
-            continue
-
-        if node_name not in index_feature_map:
-            index_feature_map.append(node_name)
+    for node_name, node_list in struc.items():                   # node_name : あるfeature ／ node_list : node_name以外のfeature
         
-        p_index = index_feature_map.index(node_name)
-        for child in node_list:
-            if child not in all_features:
-                continue
+        if node_name not in all_features:                        # ×
+            continue                                             # ×
+        if node_name not in index_feature_map:                   # ×
+            index_feature_map.append(node_name)                  # ×
+        
+        p_index = index_feature_map.index(node_name)             # p_index : 0, 1, 2, 3, ,..., 25, 26
+        
+        for child in node_list:                                  # node_list(node_name以外のfeature) の中のfeature 
+            
+            if child not in all_features:                        # ×
+                continue                                         # ×
+            if child not in index_feature_map:                   # ×
+                print(f'error: {child} not in index_feature_map')# ×
+                # index_feature_map.append(child)                # ×
 
-            if child not in index_feature_map:
-                print(f'error: {child} not in index_feature_map')
-                # index_feature_map.append(child)
-
-            c_index = index_feature_map.index(child)
+            c_index = index_feature_map.index(child)             # child の index
             # edge_indexes[0].append(p_index)
             # edge_indexes[1].append(c_index)
-            edge_indexes[0].append(c_index)
-            edge_indexes[1].append(p_index)
+            edge_indexes[0].append(c_index)                      # [1,2,3,...,25,26,0,2,3,...,25,26,0,1,3,...,25,26,...] 26child × 27
+            edge_indexes[1].append(p_index)                      # [0,0,0,..., 0, 0,1,1,1,..., 1, 1,2,2,2,..., 2, 2,...] 26      × 27
         
-
-    
-    return edge_indexes
+    return edge_indexes                                          # (2, 702)
